6f115a547c93314ee5479583fe2183ec39a2d70a
thkimwbe <ilan.earth@gmail.com>
2025-05-17 22:37:13 +0300

    created qubic interpolation downscale to compare with area inter

--- test_images_downscaler.py
+++ test_images_downscaler.py
@@ -2,10 +2,10 @@ import cv2
 import glob
 
 ORIGINAL_IMG_PATH = "test_images\\urban100\\"
-DOWNSCALED_IMG_PATH = "test_images\\urban100x4\\"
+DOWNSCALED_IMG_PATH = "test_images\\urban100x2\\"
 for img_path in glob.glob(f"{ORIGINAL_IMG_PATH}*.png"):
     img = cv2.imread(img_path)
     h, w = img.shape[:2]
-    lr_img = cv2.resize(img, (w//4, h//4), interpolation=cv2.INTER_AREA)
-    out_path = DOWNSCALED_IMG_PATH + img_path.split("\\")[-1].split(".")[0] + ".png"
+    lr_img = cv2.resize(img, (w//4, h//4), interpolation=cv2.INTER_CUBIC)
+    out_path = DOWNSCALED_IMG_PATH + img_path.split("\\")[-1].split(".")[0] + "x2.png"
     cv2.imwrite(out_path, lr_img)
